d7c34be4417fe2b5b6d0e99faedb0be40d9f0c3f
Mike Barkas <mike@barkas.com>
2016-04-16 18:01:36 -0400

    Open map by command line.

--- mapit.py
+++ mapit.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 
+import webbrowser, sys
+
+
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+    webbrowser.open('https://www.google.com/maps/place/' + address)
+else:
+    print('You need to supply a location..')
